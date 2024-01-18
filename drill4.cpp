58d2a9f2c43309dd2d04b6e7834084dba317164b
Eduardo Kunzel Teixeira <eduardo.kunzel@hotmail.com>
2024-01-18 09:06:26 -0300

    drill page 126

---
+++ drill4.cpp
@@ -0,0 +1,20 @@
+// drill page 159 ch 04
+
+#include <iostream>
+
+using namespace std;
+// adicionar while loop com regra de saída
+
+int main() {
+    int value1, value2;
+    
+        
+    cout<<"Enter the first value: ";
+    cin>> value1;
+
+    cout<<"\nEnter the second value: ";
+    cin>> value2;
+    
+    cout<<"Values: "<<value1<<"\tand\t"<<value2<<'\n';
+    
+}
