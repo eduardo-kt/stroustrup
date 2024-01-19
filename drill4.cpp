04517a2ca8c653879402720510b566eba528859b
Eduardo Kunzel Teixeira <eduardo.kunzel@hotmail.com>
2024-01-18 21:44:44 -0300

    drill p. 126 add while loop

--- drill4.cpp
+++ drill4.cpp
@@ -1,20 +1,29 @@
 // drill page 159 ch 04
 
 #include <iostream>
+#include <sstream>
+#include <string>
 
-using namespace std;
 // adicionar while loop com regra de saída
+
 
 int main() {
     int value1, value2;
-    
-        
-    cout<<"Enter the first value: ";
-    cin>> value1;
-
-    cout<<"\nEnter the second value: ";
-    cin>> value2;
-    
-    cout<<"Values: "<<value1<<"\tand\t"<<value2<<'\n';
-    
+    std::string chave;
+    while (true) {
+        std::cout<<"Enter the first value: ";
+        std::cin>> chave;
+        if (chave == "|") {
+            break;
+        }
+        std::stringstream(chave) >> value1;
+        std::cout<<"\nEnter the second value: ";
+        std::cin>> chave;
+        if (chave == "|") {
+            break;
+        }
+        std::stringstream(chave) >> value2;
+        std::cout<<"\nValues: " << value1 << "\tand\t" << value2 << "\n";
+    }
 }
+    
