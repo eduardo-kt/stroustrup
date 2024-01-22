4adb62d40a54f58a67f03cfe9dd71cc0f42a6fa3
Eduardo Kunzel Teixeira <eduardo.kunzel@hotmail.com>
2024-01-22 09:42:35 -0300

    item 7 page 159

--- drill4.cpp
+++ drill4.cpp
@@ -3,42 +3,217 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <limits>
-#include <cmath>
 
-
-
-
-
-int main() {
-    double value, smallest = std::numeric_limits<double>::quiet_NaN(), largest;
-    std::string chave;
-
-    while (true) {
-        std::cout<<"Enter a value: ";
-        std::cin>> chave;
-        if (chave == "|") {
-            break;
-        }
-
-        std::stringstream(chave) >> value;
-
-        if (std::isnan(smallest)){
-            smallest = value;
-            largest = value;
-
-        }
-
-        if (value < smallest) {
-            smallest = value;
-
-        }
-        else if (value > largest) {
-            largest = value;
-        }
-
-        std::cout<<"\nThe smallest so far: "<<smallest<<"\nThe largest so far: "<<largest<<"\n";
-                
-    }
-}
+using namespace std;
     
+int main(){
+   string key;
+   double temp, smallest, largest;
+   int counter = 0;
+   string unit;
+
+   while (true) {
+    cout<<"Enter a number followed by its unit(valid: cm, in, ft, m) \n\t(digit '|' to exit): ";
+    cin>>key;
+    if (key == "|") {
+        break;
+    }
+    istringstream iss(key);
+    iss >> temp;
+    getline(iss >> ws, unit);
+
+    if (unit == "m"){
+        temp = temp * 100;
+        cout<<"Converting meters (m) to centimeters (cm)\n";
+        if (counter == 0) {  
+        smallest = temp;        
+        ++counter;
+        }
+        else if (counter == 1){    
+            if (temp<smallest) {
+                largest = smallest;
+                smallest = temp;
+                ++counter;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+            }
+            else if (temp>smallest) {
+                largest = temp;
+                ++counter;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';            
+            }
+            else {
+                largest = smallest;
+                ++counter;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+            }
+            }
+        else {
+            if (temp<smallest) {
+                smallest = temp;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+                ++counter;
+            }
+            else if (temp>largest) {
+                largest = temp;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+                ++counter;
+            }
+            else {
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+                ++counter;
+        }
+        }
+
+    }
+    else if (unit == "in") {
+        temp = 2.54 * temp;
+        cout<<"Converting inches (in) to centimeters (cm)\n";
+        if (counter == 0) {  
+        smallest = temp;        
+        ++counter;
+        }
+        else if (counter == 1){    
+            if (temp<smallest) {
+                largest = smallest;
+                smallest = temp;
+                ++counter;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+            }
+            else if (temp>smallest) {
+                largest = temp;
+                ++counter;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';            
+            }
+            else {
+                largest = smallest;
+                ++counter;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+            }
+            }
+        else {
+            if (temp<smallest) {
+                smallest = temp;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+                ++counter;
+            }
+            else if (temp>largest) {
+                largest = temp;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+                ++counter;
+            }
+            else {
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+                ++counter;
+        }
+        }
+        
+    }
+    else if (unit == "ft") {
+        temp = 12 * 2.54 * temp;
+        cout<<"Converting feet (ft) to centimeters (cm)\n";
+        if (counter == 0) {  
+        smallest = temp;        
+        ++counter;
+        }
+        else if (counter == 1){    
+            if (temp<smallest) {
+                largest = smallest;
+                smallest = temp;
+                ++counter;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+            }
+            else if (temp>smallest) {
+                largest = temp;
+                ++counter;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';            
+            }
+            else {
+                largest = smallest;
+                ++counter;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+            }
+            }
+        else {
+            if (temp<smallest) {
+                smallest = temp;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+                ++counter;
+            }
+            else if (temp>largest) {
+                largest = temp;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+                ++counter;
+            }
+            else {
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+                ++counter;
+        }
+        }       
+
+    }
+    else {
+        if (counter == 0) {  
+        smallest = temp;        
+        ++counter;
+        }
+        else if (counter == 1){    
+            if (temp<smallest) {
+                largest = smallest;
+                smallest = temp;
+                ++counter;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+            }
+            else if (temp>smallest) {
+                largest = temp;
+                ++counter;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';            
+            }
+            else {
+                largest = smallest;
+                ++counter;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+            }
+            }
+        else {
+            if (temp<smallest) {
+                smallest = temp;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+                ++counter;
+            }
+            else if (temp>largest) {
+                largest = temp;
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+                ++counter;
+            }
+            else {
+                cout<<"The smallest number so far: "<< smallest<<'\n';
+                cout<<"The largest number so far: "<< largest<<'\n';
+                ++counter;
+        }
+        };
+    }
+   }
+}
